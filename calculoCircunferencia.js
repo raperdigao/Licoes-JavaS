d32814be24472d988cc45132d15503bcf04b4317
Raphael Pagni Perdigão <110313895+raperdigao@users.noreply.github.com>
2023-11-18 18:00:52 -0300

    Lições de java script aula dia 18112023

---
+++ calculoCircunferencia.js
@@ -0,0 +1,8 @@
+// Calculo de circunferencia
+// Pi * R ao quadrado
+// r = 16
+
+let pi = 3.14
+let raio = 16
+let total = pi * raio * raio
+console.log(`O valor é: ${total}`);
---
+++ script01.js
@@ -0,0 +1,38 @@
+// trabalhar com variáveis, console.log
+
+// console.log("carro");
+// console.log("renault");
+// console.log("sandero");
+// console.log("vermelha");
+// variavel number
+// console.log(2010);
+
+// Utilizar padrão camelCase
+// variavel(let, var, const) identificador dados
+const carro = "4X4"
+const fabricanteCarro = "renault"
+const modeloCarro = "sandero"
+const corCarro = "vermelha"
+const anoCarro = 2010
+
+console.log(carro);
+console.log(fabricanteCarro);
+console.log(modeloCarro);
+console.log(corCarro);
+console.log(anoCarro);
+
+// Concatenar a saida
+
+console.log("Carro: " + carro + " | fabricante: " + fabricanteCarro + " | modelo: " + modeloCarro + " | cor do carro: " + corCarro + " | ano do carro: " + anoCarro);
+
+// Utilizando Interpolação
+console.log(`Meu carro: ${carro}, | fabricante do carro: ${fabricanteCarro}, | modelo do carro: ${modeloCarro}, | cor do carro: ${corCarro}, | ano do carro: ${anoCarro}`);
+
+// Calculo de circunferencia
+// Pi * R ao quadrado
+// r = 16
+
+let pi = 3.14
+let raio = 16
+let total = pi * raio * raio
+console.log(`O valor é: ${total}`);
